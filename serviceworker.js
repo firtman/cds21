e83ad06c7bb94c5ae4a91b2e21e1dafd4a0f3425
Maximiliano Firtman <firtman@gmail.com>
2021-11-10 18:40:04 -0300

    stale while revalidate

--- serviceworker.js
+++ serviceworker.js
@@ -9,22 +9,41 @@ self.addEventListener("install", event => {
     });
 });
 
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then( response => {
+                // Even if the response is in the cache, we fetch it
+                // and update the cache for future usage
+                var fetchPromise = fetch(event.request).then(
+                     networkResponse => {
+                        caches.open(assetsName).then(function(cache) {
+                            cache.put(event.request, networkResponse.clone());
+                            return networkResponse;
+                        });
+                    });
+                // We use the currently cached version if it's there
+                return response || fetchPromise;
+            })
+        );
+    }); 
+
 
 // Cache First 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)    // searching in the cache
-            .then( response => {
-                if (response) {
-                    // The request is in the cache, CACHE HIT
-                    return response;
-                } else {
-                    // We need to go to the network, CACHE MISS 
-                    return fetch(event.request);
-                }
-            })
-    );
-});
+// self.addEventListener("fetch", event => {
+//     event.respondWith(
+//         caches.match(event.request)    // searching in the cache
+//             .then( response => {
+//                 if (response) {
+//                     // The request is in the cache, CACHE HIT
+//                     return response;
+//                 } else {
+//                     // We need to go to the network, CACHE MISS 
+//                     return fetch(event.request);
+//                 }
+//             })
+//     );
+// });
 
 
 // Synthesizing responses
