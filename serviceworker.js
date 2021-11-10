13d18399e6685220e9759daaaf76090ea8032bb1
Maximiliano Firtman <firtman@gmail.com>
2021-11-10 18:19:39 -0300

    precaching assets

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,13 @@
+const assetsName = "PWA_ASSETS";
+const urls = ["/", "/js/app.js", "/js/handlers.js", "/data/activites.json"];
+
+// Install the assets
+self.addEventListener("install", event => {
+    caches.open(assetsName).then( cache => {
+        cache.addAll(urls);
+    });
+});
+
 self.addEventListener("fetch", event => {
 
 });
